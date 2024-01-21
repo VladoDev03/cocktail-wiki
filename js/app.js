d8275787219e9194adb551b1c1b1a6f16535dd78
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-21 04:35:21 +0200

    Added url navigation so users can send link to specific cocktail.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 const drinks = $('#drinks');
 const info = $('#info');
 
-const noInfo = $('<h1 class="no-info">Pick a Cocktail to Drink</h1>');
-info.append(noInfo);
+const hash = window.location.hash.substring(1);
+
+const loadContent = (id) => {
+    getInfo(id)
+        .then(res => {
+            const data = res.drinks[0];
+
+            const image = createImage(data);
+            const mainInfo = createCocktail(data);
+            const recipe = createRecipe(data);
+            const instructions = createInstructions(data);
+            const video = createVideo(data);
+
+            info.empty();
+
+            info.append(image);
+            info.append(mainInfo);
+            info.append(recipe);
+            info.append(instructions);
+            info.append(video);
+        });
+};
+
+if (hash) {
+    loadContent(hash);
+} else {
+    const noInfo = $('<h1 class="no-info">Pick a Cocktail to Drink</h1>');
+    info.append(noInfo);
+}
 
 getList()
     .then(res => {
@@ -10,24 +37,8 @@ getList()
             const card = createCard(drink);
 
             card.click(function () {
-                getInfo(drink.idDrink)
-                    .then(res => {
-                        const data = res.drinks[0];
-
-                        const image = createImage(data);
-                        const mainInfo = createCocktail(data);
-                        const recipe = createRecipe(data);
-                        const instructions = createInstructions(data);
-                        const video = createVideo(data);
-
-                        info.empty();
-
-                        info.append(image);
-                        info.append(mainInfo);
-                        info.append(recipe);
-                        info.append(instructions);
-                        info.append(video);
-                    });
+                window.location.hash = drink.idDrink;
+                loadContent(drink.idDrink);
             })
 
             drinks.append(card);
